f2d98917105d3ea93e60652402dc9be38ef7a3d1
yalina2024 <yusufmondal167@gmail.com>
2026-09-07 23:46:33 +0530

    Remove frontend serving from backend

--- server/server.js
+++ server/server.js
@@ -44,13 +44,7 @@ app.use('/api/settings', require('../Routes/settingsRoutes'));
 app.use('/api/activity', require('../Routes/activityRoutes'));
 app.use('/api/dashboard', require('../Routes/dashboardRoutes'));
 
-// Serve Frontend Build in Production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-  });
-}
+
 
 // Global Error Handler
 app.use((err, req, res, next) => {
